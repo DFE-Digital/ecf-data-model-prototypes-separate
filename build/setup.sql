02adad9def02389dc879b77bab738c9875301034
Tony Headford <tony@binarycircus.com>
2024-03-21 15:25:23 +0000

    Trying some different names and training course

--- build/setup.sql
+++ build/setup.sql
@@ -81,49 +81,50 @@ insert into lead_providers(id, name) values
   (3, 'TF')
 ;
 
-create table tenureships (
+create table rosters (
   id integer primary key,
   participant_id integer references participants(id),
   school_id integer references schools(id),
+  role varchar(64),
   started_on date not null,
   finished_on date null
 );
 
-insert into tenureships(id, participant_id, school_id, started_on, finished_on) values
+insert into rosters(id, participant_id, school_id, role, started_on, finished_on) values
 
   -- ects
-  (1, 1, 1, '2021-09-01', null),
-  (2, 2, 1, '2021-09-02', '2022-09-02'),
-  (3, 3, 1, '2021-09-03', null),
-  (4, 4, 1, '2021-09-04', null),
-  (5, 5, 1, '2021-09-05', null),
-  (6, 6, 1, '2021-09-04', null),
-  (7, 7, 1, '2021-09-05', null),
-  (8, 8, 2, '2021-09-06', null),
+  (1, 1, 1, 'ect', '2021-09-01', null),
+  (2, 2, 1, 'ect', '2021-09-02', '2022-09-02'),
+  (3, 3, 1, 'ect', '2021-09-03', null),
+  (4, 4, 1, 'ect', '2021-09-04', null),
+  (5, 5, 1, 'ect', '2021-09-05', null),
+  (6, 6, 1, 'ect', '2021-09-04', null),
+  (7, 7, 1, 'ect', '2021-09-05', null),
+  (8, 8, 2, 'ect', '2021-09-06', null),
 
-  (9, 9, 2, '2022-09-01', null),
-  (10, 10, 2, '2022-09-02', null),
-  (11, 11, 2, '2022-09-03', null),
-  (12, 12, 2, '2022-09-04', null),
-  (13, 13, 2, '2022-09-05', '2022-12-04'),
+  (9, 9, 2, 'ect', '2022-09-01', null),
+  (10, 10, 2, 'ect', '2022-09-02', null),
+  (11, 11, 2, 'ect', '2022-09-03', null),
+  (12, 12, 2, 'ect', '2022-09-04', null),
+  (13, 13, 2, 'ect', '2022-09-05', '2022-12-04'),
 
   -- mentors
-  (20, 20, 1, '2021-09-05', '2022-09-05'),
-  (21, 21, 1, '2021-09-05', null),
-  (22, 22, 1, '2021-09-05', null),
-  (23, 23, 1, '2021-09-05', null),
+  (20, 20, 1, 'mentor', '2021-09-05', '2022-09-05'),
+  (21, 21, 1, 'mentor', '2021-09-05', null),
+  (22, 22, 1, 'mentor', '2021-09-05', null),
+  (23, 23, 1, 'mentor', '2021-09-05', null),
 
   -- subsequent
-  (30, 2, 2, '2022-09-09', '2023-04-05'),
-  (31, 2, 4, '2023-06-01', null),
-  (32, 8, 3, '2022-09-09', null),
-  (33, 13, 4, '2023-01-10', '2024-02-03')
+  (30, 2, 2, 'ect', '2022-09-09', '2023-04-05'),
+  (31, 2, 4, 'ect', '2023-06-01', null),
+  (32, 8, 3, 'ect', '2022-09-09', null),
+  (33, 13, 4, 'ect', '2023-01-10', '2024-02-03')
 ;
 
 create table mentorships (
   id integer primary key,
-  mentor_id integer references tenureships(id) null,
-  mentee_id integer references tenureships(id) not null,
+  mentor_id integer references rosters(id) null,
+  mentee_id integer references rosters(id) not null,
   started_on date not null,
   finished_on date null
 );
@@ -193,27 +194,39 @@ insert into provider_relationships(id, delivery_partner_id, lead_provider_id, co
   (13,  3, 3, 2023)
 ;
 
-create table participants_provider_relationships (
+create table provider_materials (
   id integer primary key,
+  name varchar(64) not null
+);
+
+insert into provider_materials(id, name) values
+  (1, 'Core Provider'),
+  (2, 'Amazing Materials')
+;
+
+create table training_course (
+  id integer primary key,
+  training_type varchar(64) not null,
   provider_relationship_id integer references provider_relationships(id),
+  provider_materials_id integer references provider_materials(id),
   participant_id integer references participants(id),
   started_on date not null,
   finished_on date null
 );
 
-insert into participants_provider_relationships(id, provider_relationship_id, participant_id, started_on, finished_on) values
-  (1, 1, 1, '2022-09-08', null),
-  (2, 1, 2, '2022-09-09', null),
-  (3, 1, 3, '2022-09-10', null),
-  (4, 1, 4, '2022-09-11', null),
-  (5, 3, 5, '2022-09-12', '2023-09-14'),
-  (6, 3, 6, '2022-09-13', null),
-  (7, 3, 7, '2022-09-13', null),
+insert into training_course(id, training_type, provider_relationship_id, participant_id, provider_materials_id, started_on, finished_on) values
+  (1, 'fip', 1, 1, null, '2022-09-08', null),
+  (2, 'fip', 1, 2, null, '2022-09-09', null),
+  (3, 'fip', 1, 3, null, '2022-09-10', null),
+  (4, 'fip', 1, 4, null, '2022-09-11', null),
+  (5, 'fip', 3, 5, null, '2022-09-12', '2023-09-14'),
+  (6, 'fip', 3, 6, null, '2022-09-13', null),
+  (7, 'cip', null, 7, 1, '2022-09-13', null),
 
-  (10, 10, 8, '2023-09-10', null),
-  (11, 10, 9, '2023-09-11', null),
-  (12, 11, 10, '2023-09-12', null),
-  (13, 11, 11, '2023-09-13', '2024-01-08'),
-  (14, 12, 12, '2023-09-13', null),
-  (15, 13, 13, '2023-09-13', null)
+  (10, 'fip', 10, 8, null, '2023-09-10', null),
+  (11, 'fip', 10, 9, null, '2023-09-11', null),
+  (12, 'fip', 11, 10, null, '2023-09-12', null),
+  (13, 'fip', 11, 11, null, '2023-09-13', '2024-01-08'),
+  (14, 'fip', 12, 12, null, '2023-09-13', null),
+  (15, 'fip', 13, 13, null, '2023-09-13', null)
 ;
